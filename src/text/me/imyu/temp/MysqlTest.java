2dfe06aefa66befef04b709a6c09c68d1d2089f7
im宇 <yitozji@163.com>
2017-12-01 19:28:18 +0800

    测试使用jdbc连接mysql，成功

---
+++ src/text/me/imyu/temp/MysqlTest.java
@@ -0,0 +1,29 @@
+package me.imyu.temp;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Created by imyu on 2017-12-01.
+ */
+public class MysqlTest {
+    @Test
+    public void test() throws Exception{
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/hs_base?useUnicode=true&characterEncoding=utf-8&useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CST";
+        String username = "root";
+        String password = "Aaimyu03";
+
+        Class.forName(driver);
+        Connection connection = DriverManager.getConnection(url, username, password);
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from user");
+        ResultSet rs = preparedStatement.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getString("username"));
+        }
+    }
+}
