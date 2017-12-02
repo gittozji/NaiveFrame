23beddce03bc87f9d31b9bbc41737dedc64485bf
im宇 <yitozji@163.com>
2017-12-02 13:40:03 +0800

    添加json格式转换。支持@ResponseBody\@ReruestBody。 通过一个简单的ajax请求确认成功。

--- src/main/java/me/imyu/web/TestController.java
+++ src/main/java/me/imyu/web/TestController.java
@@ -3,9 +3,13 @@ package me.imyu.web;
 import me.imyu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by imyu on 2017-11-29.
@@ -23,7 +27,16 @@ public class TestController {
     @RequestMapping(path = "/test/getAll")
     @ResponseBody
     public Object getAll() {
-        System.out.println(userService.getAll());
-        return null;
+        return userService.getAll();
+    }
+
+    @RequestMapping(path = "/test/convertMessage")
+    @ResponseBody
+    public Object convertMessage(@RequestBody Map param) {
+        System.out.println(param);
+        Map map = new HashMap();
+        map.putAll(param);
+        map.put("key", "我是value");
+        return map;
     }
 }
